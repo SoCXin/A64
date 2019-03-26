557041a6e564bb0fe1e66c66e515a25092fa0a22
qitas <qitas@qitas.cn>
2019-03-26 18:05:06 +0800

    add linux/kernel

--- linux/scripts/install_toolchain.sh
+++ linux/scripts/install_toolchain.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 #####################################
-##
+# This script is used to build A64 Linux.
+# by: Qitas
+# Date: 2019-03-26
 ## Install cross toolchain
 #####################################
 
@@ -13,7 +15,7 @@ TOOLS="$ROOT/toolchain"
 TOOLTARXZ="$ROOT/toolchain/toolchain_tar/toolchain"
 TOOLTAR="$ROOT/toolchain/toolchain.tar.gz"
 
-whiptail --title "OrangePi Build System" --msgbox "Installing Cross-Tools. Pls wait a mount." --ok-button Continue 10 40 0
+whiptail --title "A64 Build System" --msgbox "Installing Cross-Tools. Pls wait a mount." --ok-button Continue 10 40 0
 
 clear
 echo -e "\e[1;31m Uncompress toolchain.. \e[0m"
@@ -24,6 +26,6 @@ mv $ROOT/toolchain/toolchain/* $ROOT/toolchain
 rm -rf $ROOT/toolchain/toolchain
 rm -rf $TOOLTAR 
 
-whiptail --title "OrangePi Build System" --msgbox "Cross-Tools has installed." 10 40 0
+whiptail --title "A64 Build System" --msgbox "Cross-Tools has installed." 10 40 0
 
 
