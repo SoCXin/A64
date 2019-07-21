ec9fa4ecd670dc84e85f004f948db86c301a518f
qitas <qitas@qitas.cn>
2019-07-21 23:51:01 +0800

    fix add

---
+++ docs/README.md
@@ -0,0 +1,4 @@
+﻿# [全志 A64](https://github.com/sochub/A64) 
+
+##  [SoC资源平台](http://www.qitas.cn)  
+
--- linux/kernel/include/generated/compile.h
+++ linux/kernel/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 3 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#2 SMP PREEMPT Thu Jul 18 08:30:33 CST 2019"
+#define UTS_VERSION "#3 SMP PREEMPT Sun Jul 21 21:25:01 CST 2019"
 #define LINUX_COMPILE_BY "qitas"
-#define LINUX_COMPILE_HOST "Qi"
+#define LINUX_COMPILE_HOST "desktop"
 #define LINUX_COMPILER "gcc version 4.9.3 20150113 (prerelease) (Linaro GCC 4.9-2015.01-3) "
